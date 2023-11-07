d46c31c8fda9695802caafad547f1edad021e01b
Júlia Teles <juliatelesicm@gmail.com>
2023-11-07 10:05:06 -0300

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,20 @@
+USE BANCODADOS;
+
+/* UPDATE usuarios SET  id=1 WHERE id_user=3 */
+
+/* SELECT * FROM usuarios WHERE id_user='3'; */
+
+/* ALTER TABLE usuarios ADD key_id_user int NOT NULL IDENTITY(1,1) unique; */
+
+ /* SELECT * FROM usuarios; */
+
+ /* CREATE TABLE produtos(
+	id int NOT NULL,
+    nome varchar(100) NOT NULL,
+ ); */
+
+/* INSERT INTO produtos(id, nome) VALUES(1, 'laranja'); 
+INSERT INTO produtos(id, nome) VALUES(2, 'uva'), (3, 'banana'), (4, 'limão'),(5, 'melancia') ;*/
+
+
+ SELECT * FROM endereco;
